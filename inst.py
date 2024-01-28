4c2cf66dab11fe25d07a1cbc5cd92f74c8d37bba
peshk0v <nikita.peshkovvv@gmail.com>
2024-01-28 13:58:53 +0300

    swe

--- inst.py
+++ inst.py
@@ -12,7 +12,7 @@ try: os.system(f"cfdisk {devdisk}")
 finally:
     print("\nDisk partion succices!")
     os.system("lsblk")
-    print("Formating partions")
+    print("\nFormating partions")
     sleep(1)
     try: os.system(f"mkfs.fat -F32 {devdisk}p1")
     finally: 
@@ -20,3 +20,23 @@ finally:
         try: os.system(f"mkfs.fat -F32 {devdisk}p3")
         finally: 
             print("Disk formating succices!")
+            sleep(1)
+            print("\nMounting home dir")
+            try: os.system(f"mount {devdisk}p2 /mnt")
+            finally:
+                try: os.system("mkdir /mnt/home")
+                finally:
+                    try: os.system(f"mount {devdisk}p3 /mnt/home")
+                    finally: 
+                        os.system("lsblk")
+                        print("Gread")
+                        sleep(1)
+                        print("\nInstalling base")
+                        try: os.system("pacstrap -i /mnt base linux linux-firmware sudo nano")
+                        finally:
+                            try: os.system("genfstab -U -p /mnt >> /mnt/etc/fstab")
+                            finally:
+                                try: os.system("arch-chroot /mnt /bin/bash")
+                                finally: 
+                                    try: os.system("cp data/linux/locale.gen /etc/locale.gen")
+                                    finally: print("popa")
