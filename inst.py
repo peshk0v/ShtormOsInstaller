4f840de0e55cdd279b03f551b4a4837136dfe3e1
peshk0v <nikita.peshkovvv@gmail.com>
2024-01-28 13:14:24 +0300

    aa

--- inst.py
+++ inst.py
@@ -1,4 +1,5 @@
 import os
+from time import sleep
 
 print("HELLO FROM SHTORMOS!")
 print("DISK PATRION")
@@ -7,4 +8,15 @@ disk = input("\nType name of disk > ")
 devdisk = "/dev/" + disk
 print("Please partion your disk")
 input("\nTo start patrion click ENTER")
-os.system(f"cfdisk {devdisk}")
+try: os.system(f"cfdisk {devdisk}")
+finally:
+    print("\nDisk partion succices!")
+    os.system("lsblk")
+    print("Formating partions")
+    sleep(1)
+    try: os.system(f"mkfs.fat -F32 {devdisk}p1")
+    finally: 
+        os.system(f"mkfs.ext4 {devdisk}p2")
+        try: os.system(f"mkfs.fat -F32 {devdisk}p3")
+        finally: 
+            print("Disk formating succices!")
